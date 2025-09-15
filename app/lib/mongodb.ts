33f99a70fd9eeb7d79484125709f75f31d691543
kumar chandan <chandan.kumar@oyelabs.com>
2025-09-15 17:59:17 +0530

    Fixed url related error

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -3,6 +3,12 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 if (!uri) throw new Error("Please add your Mongo URI to .env.local or Vercel Environment Variables");
 
+// Add options to fix TLS error on Vercel
+const options = {
+  tls: true,
+  tlsAllowInvalidCertificates: true, // ⚠️ for testing — remove later when SSL certs are good
+};
+
 declare global {
   // This is for TypeScript to recognize the global variable in dev
   // eslint-disable-next-line no-var
@@ -14,12 +20,12 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
